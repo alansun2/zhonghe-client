ae83f6973da9734dc197645ccced67b64e7f7ef6
AlanSun <18698525802@163.com>
2021-10-14 23:04:37 +0800

    update 减少依赖，减小压缩包

--- zhonghe-server/src/main/java/vip/tuoyang/zhonghe/Application.java
+++ zhonghe-server/src/main/java/vip/tuoyang/zhonghe/Application.java
@@ -1,17 +1,18 @@
 package vip.tuoyang.zhonghe;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 /**
  * @author AlanSun
  * @date 2021/9/3 11:03
  */
-@EnableScheduling
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        new SpringApplicationBuilder(Application.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
     }
 }
