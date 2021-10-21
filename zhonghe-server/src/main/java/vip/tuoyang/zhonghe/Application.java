4fea3c36604698fa8b88c2bc9dd2a2b28cbde0ab
AlanSun <18698525802@163.com>
2021-10-21 13:30:06 +0800

    update 去除定时状态检查

--- zhonghe-server/src/main/java/vip/tuoyang/zhonghe/Application.java
+++ zhonghe-server/src/main/java/vip/tuoyang/zhonghe/Application.java
@@ -9,7 +9,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  * @author AlanSun
  * @date 2021/9/3 11:03
  */
-@EnableScheduling
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
