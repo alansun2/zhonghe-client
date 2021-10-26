a9afd8a34106fd45c216bdb5a1ca4e03cc2b793e
AlanSun <18698525802@163.com>
2021-10-26 11:15:49 +0800

    refactor 优化状态

--- zhonghe-server/src/main/java/vip/tuoyang/zhonghe/support/MyCallbackHandler.java
+++ zhonghe-server/src/main/java/vip/tuoyang/zhonghe/support/MyCallbackHandler.java
@@ -33,8 +33,6 @@ public class MyCallbackHandler implements ZhongHeCallback {
 
     private Long lastOfflineTime;
 
-    private volatile Integer lastState;
-
     private final ScheduledExecutorService scheduledExecutorService = new ScheduledThreadPoolExecutor(1, r -> {
         Thread thread = new Thread(r);
         thread.setName("MyCallbackHandler");
@@ -51,46 +49,41 @@ public class MyCallbackHandler implements ZhongHeCallback {
     public synchronized void callback(String label, SendStateHandler stateHandler) {
         final int state = stateHandler.isOnline().equals(StateEnum.ONLINE_RUNNING) ? 1 : 0;
         log.info("服务状态变更，state: [{}]", state);
-        if (lastState == null || lastState == 1 || !lastState.equals(state)) {
-            if (state == 0 && lastOfflineTime != null && lastOfflineTime + 5000 > System.currentTimeMillis()) {
-                log.info("0 抑制");
-                return;
-            }
-            ZhongHeDto<StateRequest> zhongHeBaseRequest = new ZhongHeDto<>();
-            StateRequest stateRequest = new StateRequest();
-            stateRequest.setLabel(label);
-            stateRequest.setState(state);
-            zhongHeBaseRequest.setCommand((byte) 13);
-            zhongHeBaseRequest.setData(stateRequest);
-            broadcastClient.sendMessage(zhongHeBaseRequest);
-            lastOfflineTime = System.currentTimeMillis();
-            lastState = state;
+        if (state == 0 && lastOfflineTime != null && lastOfflineTime + 5000 > System.currentTimeMillis()) {
+            log.info("0 抑制");
+            return;
+        }
+        ZhongHeDto<StateRequest> zhongHeBaseRequest = new ZhongHeDto<>();
+        StateRequest stateRequest = new StateRequest();
+        stateRequest.setLabel(label);
+        stateRequest.setState(state);
+        zhongHeBaseRequest.setCommand((byte) 13);
+        zhongHeBaseRequest.setData(stateRequest);
+        broadcastClient.sendMessage(zhongHeBaseRequest);
+        lastOfflineTime = System.currentTimeMillis();
 
-            if (state == 0 && scheduledFuture == null) {
-                scheduledFuture = scheduledExecutorService.scheduleWithFixedDelay(() -> {
-                    log.info("重连");
-                    boolean flag;
-                    try {
-                        final ZhongHeResult<StateResponse> zhongHeResultSate = zhongHeClient.state();
-                        flag = zhongHeResultSate.isSuccess() && zhongHeResultSate.getData().getState().equals(StateEnum.ONLINE_RUNNING);
-                    } catch (Exception e) {
-                        flag = false;
-                    }
+        if (state == 0 && scheduledFuture == null) {
+            scheduledFuture = scheduledExecutorService.scheduleWithFixedDelay(() -> {
+                log.info("重连");
+                boolean flag;
+                try {
+                    final ZhongHeResult<StateResponse> zhongHeResultSate = zhongHeClient.state();
+                    flag = zhongHeResultSate.isSuccess() && zhongHeResultSate.getData().getState().equals(StateEnum.ONLINE_RUNNING);
+                } catch (Exception e) {
+                    flag = false;
+                }
 
-                    if (flag) {
-                        ZhongHeDto<StateRequest> zhongHeBaseRequest1 = new ZhongHeDto<>();
-                        StateRequest stateRequest1 = new StateRequest();
-                        stateRequest1.setLabel(label);
-                        stateRequest1.setState(1);
-                        zhongHeBaseRequest1.setCommand((byte) 13);
-                        zhongHeBaseRequest1.setData(stateRequest1);
-                        broadcastClient.sendMessage(zhongHeBaseRequest1);
-                        this.cancelTask();
-                    }
-                }, 0, 1, TimeUnit.MINUTES);
-            }
-        } else {
-            log.info("状态未变化");
+                if (flag) {
+                    ZhongHeDto<StateRequest> zhongHeBaseRequest1 = new ZhongHeDto<>();
+                    StateRequest stateRequest1 = new StateRequest();
+                    stateRequest1.setLabel(label);
+                    stateRequest1.setState(1);
+                    zhongHeBaseRequest1.setCommand((byte) 13);
+                    zhongHeBaseRequest1.setData(stateRequest1);
+                    broadcastClient.sendMessage(zhongHeBaseRequest1);
+                    this.cancelTask();
+                }
+            }, 0, 1, TimeUnit.MINUTES);
         }
     }
 
